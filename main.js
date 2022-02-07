3924012d06f7f6df89c253ca1110f34a974b2234
SanviThakur <95920632+SanviThakur@users.noreply.github.com>
2022-02-07 15:47:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,25 @@ function draw(){
 function setup(){
 canvas = createCanvas(300, 300);
 canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
 
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Intialized");
+}
+
+function gotPoses(results){
+if(results.length > 0){
+    console.log(results);
+    console.log("noseX = " + results[0].pose.nose.x);
+    console.log("noseY = " + results[0].pose.nose.y);
+}
 }
 
 function take_snapshot(){
